9ce9ba439d97b6fa5b22be9fedb5aa61f73dc36c
Michael Bernasol <mfbernasol@gmail.com>
2025-02-26 21:43:40 -0600

    setup crud operations and handler

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54604958c0c3efab03875eb54da327813eed9e47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57012e0578c4009bb8623c2688a861223ed967cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
